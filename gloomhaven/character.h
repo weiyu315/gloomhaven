532c42e04cec85d5a4c0457bc91849edfa2af157
WeiYu Chen <weiyu900315@gmail.com>
2020-05-13 00:25:35 +0800

    int main

---
+++ gloomhaven/character.h
@@ -0,0 +1,13 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class player
+{
+public:
+	string name;
+	int hp;
+	int attack;
+	int shield;
+};
--- gloomhaven/main.cpp
+++ gloomhaven/main.cpp
@@ -1,2 +1,11 @@
 #include<iostream>
 #include<string>
+#include<vector>
+#include"character.h"
+#include"monster.h"
+using namespace std;
+
+int main()
+{
+	
+}
