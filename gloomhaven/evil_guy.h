34d5b71f89c3688851a8e5dff367e71b48b033d9
海靖 朱 <garyandhouse@gmail.com>
2020-05-29 20:27:37 +0800

    main檔有改

--- gloomhaven/evil_guy.h
+++ gloomhaven/evil_guy.h
@@ -1,14 +1,20 @@
 #pragma once
 #include<iostream>
 #include"evil_guy.h"
+#include"monster_card.h"
 //此為怪物的資訊
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class evil_guy {
 private:
 	string monster_name;
-	int monster_hp;
-	int monster_attack;
-	int monster_sheild;
+	int monster_hp;//
+	int monster_attack;//怪物的攻擊值
+	int monster_sheild;//護甲
+	int range;//怪物的射程
+	int x;//x跟y為怪物的位置
+	int y;
+	vector<monster_card>card;
 public:
+	
 };
--- gloomhaven/main.cpp
+++ gloomhaven/main.cpp
@@ -12,12 +12,20 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
+	int caracter_qualite;
 	string play_or_exit;
 	cin >> play_or_exit;
 	if (play_or_exit == "play")
 	{
+		cout << "請輸入出場角色數量";
+		cin >> caracter_qualite;
+		for (int i = 0; i < caracter_qualite; i++) {
+
+		}
+
 		monster a("monster1.txt");
 		map b("map1.txt");
+
 
 		//角色、怪物檔案開啟
 		/*int debug_Mode;
