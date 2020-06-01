43e4b6d5e7f4596a328b315876d16227e5234e39
WeiYu Chen <weiyu900315@gmail.com>
2020-06-01 15:49:29 +0800

    main

--- gloomhaven/main.cpp
+++ gloomhaven/main.cpp
@@ -5,6 +5,8 @@
 #include<sstream>
 #include"character.h"
 #include"monster.h"
+#include"map.h"
+#include"evil_guy.h"
 
 using namespace std;
 
@@ -17,6 +19,7 @@ int main(int argc, char* argv[])
 	cin >> play_or_exit;
 	if (play_or_exit == "play")
 	{
+		string map_file;
 		int debug_Mode;
 		characterFileReader(argv[1], Character);//角色讀檔
 		ifstream monster_inFile(argv[2], ios::in);
@@ -49,89 +52,99 @@ int main(int argc, char* argv[])
 		}
 		/*-------------------------------讀取上場角色資料-------------------------------*/
 		/*---------------------------------讀取地圖檔案---------------------------------*/
+		monster a("monster1.txt");//暫時
+		cin >> map_file;
+		map b(map_file);
+		vector<evil_guy> Monster;//怪物
+		evil_guy  re_Monster;//儲存要放進vector的怪物
+		for (int i = 0; i < b.Get_monster_quaility(); i++) {
+			if (b.Get_monster_status(i, playCharacter_amount) == 1 || b.Get_monster_status(i, playCharacter_amount) == 2) {
+				re_Monster.initialization_bad_guy(b.Get_monster_name(i), b.Get_monster_location_x(i), b.Get_monster_location_y(i), b.Get_monster_status(i, playCharacter_amount), a);
+				re_Monster.text();
+				Monster.push_back(re_Monster);
+			}
+			/*---------------------------------讀取地圖檔案---------------------------------*/
+		}
 
-		/*---------------------------------讀取地圖檔案---------------------------------*/
+		return 0;
 	}
-	
-	return 0;
 }
-
-void characterFileReader(string fileName, vector<character>& Character)
-{
-	ifstream inFile(fileName, ios::in);
-	int character_amount;
-	inFile >> character_amount;//資料庫角色數量
-	for (int i = 0; i < character_amount; i++)//讀取角色資料
+	void characterFileReader(string fileName, vector<character>& Character)
 	{
-		character newCharacter;
-		inFile >> newCharacter.name >> newCharacter.max_hp >> newCharacter.hand_card_amount;
-		inFile >> newCharacter.total_card_amount;
-		inFile.ignore();
-		for (int j = 0; j < newCharacter.total_card_amount; j++)//讀取角色的每張卡片
+		ifstream inFile(fileName, ios::in);
+		int character_amount;
+		inFile >> character_amount;//資料庫角色數量
+		for (int i = 0; i < character_amount; i++)//讀取角色資料
 		{
-			card newCard;
-			string card_line;
-			getline(inFile, card_line);
-			stringstream ss(card_line);
-			ss >> newCard.number >> newCard.dex;
-			string newSkill_name;
-			int newVal;
-			bool above = true;
-			while (ss >> newSkill_name)
+			character newCharacter;
+			inFile >> newCharacter.name >> newCharacter.max_hp >> newCharacter.hand_card_amount;
+			inFile >> newCharacter.total_card_amount;
+			inFile.ignore();
+			for (int j = 0; j < newCharacter.total_card_amount; j++)//讀取角色的每張卡片
 			{
-				skill newSkill;
-				if (newSkill_name == "-")
+				card newCard;
+				string card_line;
+				getline(inFile, card_line);
+				stringstream ss(card_line);
+				ss >> newCard.number >> newCard.dex;
+				string newSkill_name;
+				int newVal;
+				bool above = true;
+				while (ss >> newSkill_name)
 				{
-					above = false;
-					continue;
-				}
-				ss >> newVal;
-				if (newSkill_name == "range")
-				{
+					skill newSkill;
+					if (newSkill_name == "-")
+					{
+						above = false;
+						continue;
+					}
+					ss >> newVal;
+					if (newSkill_name == "range")
+					{
+						if (above)
+						{
+							newCard.skill_above[newCard.skill_above.size() - 1].range = newVal;
+						}
+						else
+						{
+							newCard.skill_below[newCard.skill_below.size() - 1].range = newVal;
+						}
+						continue;
+					}
+					newSkill.set_skill(newSkill_name, newVal);
 					if (above)
 					{
-						newCard.skill_above[newCard.skill_above.size() - 1].range = newVal;
+						newCard.skill_above.push_back(newSkill);
 					}
 					else
 					{
-						newCard.skill_below[newCard.skill_below.size() - 1].range = newVal;
+						newCard.skill_below.push_back(newSkill);
 					}
-					continue;
 				}
-				newSkill.set_skill(newSkill_name, newVal);
-				if (above)
-				{
-					newCard.skill_above.push_back(newSkill);
-				}
-				else
-				{
-					newCard.skill_below.push_back(newSkill);
-				}
+				newCharacter.total_card.push_back(newCard);
 			}
-			newCharacter.total_card.push_back(newCard);
+			Character.push_back(newCharacter);
 		}
-		Character.push_back(newCharacter);
+		inFile.close();
 	}
-	inFile.close();
-}
 
-/*-------------------------------character資料庫輸出-------------------------------*/
-		//characterFileReader("character1.txt", Character);
-		//for (int i = 0; i < Character.size(); i++)
-		//{
-		//	cout << Character[i].name << " " << Character[i].max_hp << " " << Character[i].hand_card_amount << endl;
-		//	for (int j = 0; j < Character[i].total_card_amount; j++)
-		//	{
-		//		cout << Character[i].total_card[j].number << " " << Character[i].total_card[j].dex << " ";
-		//		for (int m = 0; m < Character[i].total_card[j].skill_above.size(); m++)
-		//		{
-		//			cout << Character[i].total_card[j].skill_above[m].type << " " << Character[i].total_card[j].skill_above[m].value << " ";
-		//		}
-		//		cout << "- ";
-		//		for (int n = 0; n < Character[i].total_card[j].skill_below.size(); n++)
-		//		{
-		//			cout << Character[i].total_card[j].skill_below[n].type << " " << Character[i].total_card[j].skill_below[n].value << " ";
-		//		}
-		//		cout << endl;
-		//	}
-		//}
+	/*-------------------------------character資料庫輸出-------------------------------*/
+			//characterFileReader("character1.txt", Character);
+			//for (int i = 0; i < Character.size(); i++)
+			//{
+			//	cout << Character[i].name << " " << Character[i].max_hp << " " << Character[i].hand_card_amount << endl;
+			//	for (int j = 0; j < Character[i].total_card_amount; j++)
+			//	{
+			//		cout << Character[i].total_card[j].number << " " << Character[i].total_card[j].dex << " ";
+			//		for (int m = 0; m < Character[i].total_card[j].skill_above.size(); m++)
+			//		{
+			//			cout << Character[i].total_card[j].skill_above[m].type << " " << Character[i].total_card[j].skill_above[m].value << " ";
+			//		}
+			//		cout << "- ";
+			//		for (int n = 0; n < Character[i].total_card[j].skill_below.size(); n++)
+			//		{
+			//			cout << Character[i].total_card[j].skill_below[n].type << " " << Character[i].total_card[j].skill_below[n].value << " ";
+			//		}
+			//		cout << endl;
+			//	}
+			//}
