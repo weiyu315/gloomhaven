575036c167dc5574bb8921c4978cd5a7d68251f2
WeiYu Chen <weiyu900315@gmail.com>
2020-06-10 03:56:11 +0800

    main

--- gloomhaven/character.cpp
+++ gloomhaven/character.cpp
@@ -19,7 +19,7 @@ character::character(string newName, int newMax_hp, int newHand_card_amount, int
 	this->total_card_amount = newTotal_card_amount;//取得總共牌數
 	//this->total_card.resize(this->total_card_amount);
 }
-void character::setUsing_card(int index, int newUsing_card_number, card newUsing_card[2], vector<card> newHand_card)
+bool character::setUsing_card(int index, int newUsing_card_number, card newUsing_card[2], vector<card> newHand_card)
 {
 	int i = 0;
 	bool exist = true;
@@ -29,12 +29,14 @@ void character::setUsing_card(int index, int newUsing_card_number, card newUsing
 		if (i >= newHand_card.size())
 		{
 			exist = false;
-			cout << "Warning: Card number isn't exist." << endl;
+			cout << "Warning: Card number isn't exist in hand card." << endl;
 			break;
 		}
 	}
 	if (exist)
 	{
 		newUsing_card[index] = newHand_card[i];
+		return true;
 	}
+	return false;
 }
