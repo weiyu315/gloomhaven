2f3fe4426db4dad5f1af8c6964c690906f0bb3b6
海靖 朱 <garyandhouse@gmail.com>
2020-06-10 13:57:02 +0800

    main

--- gloomhaven/main.cpp
+++ gloomhaven/main.cpp
@@ -453,6 +453,7 @@ int main(int argc, char* argv[])
 										case 2://move val
 											cout << "move command: ";
 											Map.move(playCharacter[j].map_name, s.value, Monster);
+											Map.output(Monster);
 											break;
 										case 3://heal val
 											if (playCharacter[j].round_hp > playCharacter[j].max_hp) { playCharacter[j].round_hp = playCharacter[j].max_hp; }
